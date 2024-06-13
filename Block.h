c2b89c3f951168686280a8a7d5becbed315f480d
tyler-grimes <tyler.d.grimes@gmail.com>
2024-06-12 18:21:55 -0600

    block pretty much done

--- Block.h
+++ Block.h
@@ -1,14 +1,29 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
-class Block()
+#include <chrono>
+
+class Block
 {
 private:
     int blockSize;
     unsigned char *memoryPointer;
-    int *byteData[blockSize];
+    unsigned char *byteData;
+    int tag;
+    bool isValid;
+    bool isDirty;
+
+    std::chrono::high_resolution_clock m_clock;
+    long timestamp;
 
 public:
+    Block(int blockSize, unsigned char *memoryPointer);
+    void read(unsigned long blockOffset, unsigned char &data);
+    void write(unsigned long blockOffset, unsigned char data);
+    void loadFromMemory();
+    void saveToMemory();
+    void updateTimestamp();
+    void display();
 };
 
 #endif
